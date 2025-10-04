4bafd13cb2e362a50a5f924edeaed527044c3561
taitami <vladm2329@gmail.com>
2025-10-04 20:27:12 +0300

    refactor: changed types for ids

--- docs/interfaces.ts
+++ docs/interfaces.ts
@@ -30,95 +30,95 @@ enum specialty_name {
 }
 
 interface ContactData {
-  contact_data_id: number;
+  contact_data_id: string;
   phone: string;
   email: string;
 }
 
 interface Client {
-  client_id: number;
+  client_id: string;
   first_name: string;
   last_name: string;
   gender: gender_name;
-  contact_data_id: number; // FK -> ContactData
+  contact_data_id: string; // FK -> ContactData
 }
 
 interface Membership {
-  membership_id: number;
+  membership_id: string;
   start_date: Date;
   end_date: Date;
   price: number;
   status: membership_status;
   is_disposable: boolean;
-  client_id: number;       // FK -> Client
-  class_type_id: number;   // FK -> ClassType
+  client_id: string;       // FK -> Client
+  class_type_id: string;   // FK -> ClassType
 }
 
 interface Payment {
-  payment_id: number;
+  payment_id: string;
   timestamp: Date;
   amount: number;
   status: payment_status;
   method: payment_method;
-  client_id: number;       // FK -> Client
-  membership_id: number;   // FK -> Membership
+  client_id: string;       // FK -> Client
+  membership_id: string;   // FK -> Membership
 }
 
 interface Qualification {
-  trainer_id: number;      // FK -> Trainer
-  class_type_id: number;   // FK -> ClassType
+  trainer_id: string;      // FK -> Trainer
+  class_type_id: string;   // FK -> ClassType
 }
 
 interface Gym {
-  gym_id: number;
+  gym_id: string;
   address: string;
   gym_capacity: number;
 }
 
 interface Trainer {
-  trainer_id: number;
+  trainer_id: string;
   first_name: string;
   last_name: string;
   specialty: specialty_name;
-  contact_data_id: number; // FK -> ContactData
+  contact_data_id: string; // FK -> ContactData
 }
 
 interface TrainerPlacement {
-  trainer_id: number;      // FK -> Trainer
-  gym_id: number;          // FK -> Gym
+  trainer_id: string;      // FK -> Trainer
+  gym_id: string;          // FK -> Gym
 }
 
 interface ClassSession {
-  session_id: number;
+  session_id: string;
   capacity: number;
   date: Date;
   duration: number;        
-  trainer_id: number;      // FK -> Trainer
-  class_type_id: number;   // FK -> ClassType
-  room_id: number;         // FK -> Room
+  trainer_id: string;      // FK -> Trainer
+  class_type_id: string;   // FK -> ClassType
+  room_id: string;         // FK -> Room
 }
 
 interface Attendance {
-  session_id: number;      // FK -> ClassSession
-  client_id: number;       // FK -> Client
+  session_id: string;      // FK -> ClassSession
+  client_id: string;       // FK -> Client
   status: string;
 }
 
 interface ClassType {
-  class_type_id: number;
+  class_type_id: string;
   name: string;
   description: string;
   level: string;
 }
 
 interface Room {
-  room_id: number;
+  room_id: string;
   capacity: number;
-  gym_id: number;          // FK -> Gym
+  gym_id: string;          // FK -> Gym
 }
 
 interface RoomClassType {
-  room_id: number;         // FK -> Room
-  class_type_id: number;   // FK -> ClassType
+  room_id: string;         // FK -> Room
+  class_type_id: string;   // FK -> ClassType
   name: string;
 }
