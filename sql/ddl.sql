1b6fe35431460d439be7416cfff4bb868ffc50f7
Ann Kuts <kuutsann@gmail.com>
2025-10-06 21:14:40 +0300

    feat: extend ddl with class_type, class_session, room_class_type, qualification and trainer_placement entities

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,3 +1,30 @@
+DO
+$$
+  BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'gender_name') THEN
+      CREATE TYPE gender_name AS ENUM ('male', 'female');
+    END IF;
+  END
+$$;
+
+DO
+$$
+  BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'specialty_name') THEN
+      CREATE TYPE specialty_name AS ENUM ('workout', 'yoga', 'swimming pool');
+    END IF;
+  END
+$$;
+
+DO
+$$
+  BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'level_name') THEN
+      CREATE TYPE level_name AS ENUM ('beginner', 'intermediate', 'advanced');
+    END IF;
+  END
+$$;
+
 CREATE TABLE IF NOT EXISTS contact_data
 (
   contact_data_id serial PRIMARY KEY,
@@ -5,28 +32,14 @@ CREATE TABLE IF NOT EXISTS contact_data
   email varchar(32) NOT NULL UNIQUE
 );
 
-DO $$
-BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'gender_name') THEN
-    CREATE TYPE gender_name AS ENUM ('male', 'female');
-  END IF;
-END$$;
-
 CREATE TABLE IF NOT EXISTS client
 (
   client_id serial PRIMARY KEY,
   first_name varchar(32) NOT NULL,
   last_name varchar(32) NOT NULL,
   gender gender_name NOT NULL,
-  contact_data_id integer NOT NULL references contact_data(contact_data_id)
+  contact_data_id integer NOT NULL references contact_data (contact_data_id)
 );
-
-DO $$
-BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'specialty_name') THEN
-    CREATE TYPE specialty_name AS ENUM ('workout', 'yoga', 'swimming pool');
-  END IF;
-END$$;
 
 CREATE TABLE IF NOT EXISTS trainer
 (
@@ -34,10 +47,10 @@ CREATE TABLE IF NOT EXISTS trainer
   first_name varchar(32) NOT NULL,
   last_name varchar(32) NOT NULL,
   specialty specialty_name NOT NULL,
-  contact_data_id integer NOT NULL references contact_data(contact_data_id)
+  contact_data_id integer NOT NULL references contact_data (contact_data_id)
 );
 
-CREATE TABLE IF NOT EXISTS gym 
+CREATE TABLE IF NOT EXISTS gym
 (
   gym_id serial PRIMARY KEY,
   address varchar(60) NOT NULL UNIQUE,
@@ -48,5 +61,49 @@ CREATE TABLE IF NOT EXISTS room
 (
   room_id serial PRIMARY KEY,
   capacity integer NOT NULL CHECK (capacity > 0),
-  gym_id integer NOT NULL references gym(gym_id)
+  gym_id integer NOT NULL references gym (gym_id)
+);
+
+CREATE TABLE IF NOT EXISTS class_type
+(
+  class_type_id serial PRIMARY KEY,
+  name varchar(32) NOT NULL UNIQUE,
+  description TEXT,
+  level level_name NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS room_class_type
+(
+  room_id integer NOT NULL references room (room_id),
+  class_type_id integer NOT NULL references class_type (class_type_id),
+  name varchar(64),
+  PRIMARY KEY (room_id, class_type_id)
+);
+
+CREATE TABLE IF NOT EXISTS class_session
+(
+  session_id serial PRIMARY KEY,
+  trainer_id integer NOT NULL references trainer (trainer_id),
+  room_id integer NOT NULL,
+  class_type_id integer NOT NULL,
+  duration interval NOT NULL CHECK (duration > interval '0'),
+  capacity integer NOT NULL CHECK (capacity > 0),
+  --capacity should be less than or equal to room capacity
+  date date NOT NULL,
+  FOREIGN KEY (room_id, class_type_id)
+    REFERENCES room_class_type (room_id, class_type_id)
+);
+
+CREATE TABLE IF NOT EXISTS qualification
+(
+  trainer_id integer NOT NULL references trainer (trainer_id),
+  class_type_id integer NOT NULL references class_type (class_type_id),
+  PRIMARY KEY (trainer_id, class_type_id)
+);
+
+CREATE TABLE IF NOT EXISTS trainer_placement
+(
+  trainer_id integer NOT NULL references trainer (trainer_id),
+  gym_id integer NOT NULL references gym (gym_id),
+  PRIMARY KEY (trainer_id, gym_id)
 );
