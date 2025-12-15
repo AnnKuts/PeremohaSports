57f64700e8e4cd33caab6cb255cef0c7a6e8d6f5
Ann Kuts <kuutsann@gmail.com>
2025-12-15 04:52:43 +0200

    chore: fix dependencies and move routes

--- course-work/src/app.ts
+++ course-work/src/app.ts
@@ -4,11 +4,6 @@ import helmet from "helmet";
 import morgan from "morgan";
 
 import type MessageResponse from "./interfaces/message-response.js";
-import registerRoutes from "./routes/register.routes.js";
-import clientsRoutes from "./routes/clients.routes.js";
-import membershipsRoutes from "./routes/memberships.routes.js";
-import paymentsRoutes from "./routes/payments.routes.js";
-import authRoutes from "./routes/auth.routes.js";
 
 import api from "./api/index.js";
 import * as middlewares from "./middlewares.js";
@@ -19,19 +14,14 @@ app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use('/register', registerRoutes);
-app.use("/clients", clientsRoutes);
-app.use("/memberships", membershipsRoutes);
-app.use("/payments", paymentsRoutes);
-app.use("/auth", authRoutes);
 
 app.get<object, MessageResponse>("/", (req, res) => {
   res.json({
-    message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
+    message: "peremoha bude",
   });
 });
 
-app.use("/api/v1", api);
+app.use("/", api);
 
 app.get<object, MessageResponse>("/", (req, res) => {
   res.json({
