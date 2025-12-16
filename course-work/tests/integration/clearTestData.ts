c865f505168e7bdd6dd562472bf0d0a5e999cf57
Ann Kuts <kuutsann@gmail.com>
2025-12-16 09:27:05 +0200

    chore: fix seed

--- course-work/tests/integration/clearTestData.ts
+++ course-work/tests/integration/clearTestData.ts
@@ -1,14 +1,19 @@
 import prisma from "../../src/lib/prisma";
 
 export async function clearTestData() {
-  await prisma.attendance.deleteMany({});
-  await prisma.class_session.deleteMany({});
-  await prisma.payment.deleteMany({});
-  await prisma.membership.deleteMany({});
-  await prisma.room_class_type.deleteMany({});
-  await prisma.trainer_placement.deleteMany({});
-  await prisma.room.deleteMany({});
-  await prisma.qualification.deleteMany({});
-  await prisma.class_type.deleteMany({});
-  await prisma.gym.deleteMany({});
+  await prisma.$transaction([
+    prisma.attendance.deleteMany(),
+    prisma.payment.deleteMany(),
+    prisma.class_session.deleteMany(),
+    prisma.membership.deleteMany(),
+    prisma.room_class_type.deleteMany(),
+    prisma.qualification.deleteMany(),
+    prisma.trainer_placement.deleteMany(),
+    prisma.trainer.deleteMany(),
+    prisma.client.deleteMany(),
+    prisma.room.deleteMany(),
+    prisma.class_type.deleteMany(),
+    prisma.gym.deleteMany(),
+    prisma.contact_data.deleteMany(),
+  ]);
 }
